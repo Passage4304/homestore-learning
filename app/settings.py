48ed7297461e2c461553821bcd62f41118139034
Artem <n410tsacc@gmail.com>
2026-01-18 12:09:08 +0700

    develop(app:app): installing debug toolbar

--- app/settings.py
+++ app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'debug_toolbar',
 
     'main',
     'goods',
@@ -51,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -119,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
--- app/urls.py
+++ app/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# from debug_toolbar.toolbar import debug_toolbar_urls
+
+from app.settings import DEBUG
 
 
 urlpatterns = [
@@ -23,3 +26,8 @@ urlpatterns = [
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='catalog'))
 ]
+
+if DEBUG:
+    urlpatterns += [
+        path("__debug__/", include('debug_toolbar.urls')),
+    ]
