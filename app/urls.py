80fe1d280c8b454e69a775dd1148e26a20b2f5a8
Artem <n410tsacc@gmail.com>
2026-01-18 13:45:59 +0700

    develop(app:goods): registered simple tags for categories

--- app/urls.py
+++ app/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 # from debug_toolbar.toolbar import debug_toolbar_urls
 
-from app.settings import DEBUG
+from app import settings
 
 
 urlpatterns = [
@@ -27,7 +28,8 @@ urlpatterns = [
     path('catalog/', include('goods.urls', namespace='catalog'))
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
-        path("__debug__/", include('debug_toolbar.urls')),
+        path('__debug__/', include('debug_toolbar.urls')),
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
